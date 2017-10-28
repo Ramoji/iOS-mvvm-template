87688604064216715bbaddaa961c4e31dcbb472d
brianbolze <brianbolze@gmail.com>
2017-10-28 16:07:16 -0700

    Modified software license in file headers

--- MVVM-Template/MVVM-Template/AppDelegate.swift
+++ MVVM-Template/MVVM-Template/AppDelegate.swift
@@ -3,11 +3,7 @@
 //  MVVM-Template
 //
 //  Created by BrianBolze on 10/28/17.
-//  Copyright © 2017 brianbolze.
-//
-//  This work is free. You can redistribute it and/or modify it under the
-//  terms of the Do What The Fuck You Want To Public License, Version 2,
-//  as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+//  Copyright © 2017 brianbolze. All rights reserved.
 //
 
 import UIKit
